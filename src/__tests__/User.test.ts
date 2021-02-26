2b62676553b29d103cf07b84e5c5810bd49e6876
emmanuel <oliveiravianaviana@gmail.com>
2021-02-26 14:06:36 -0300

    testes finalizado

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -16,6 +16,15 @@ describe("Users", () =>{
     });
 
     expect(response.status).toBe(201)
-    })
+    });
+
+    it("Shoul not to be able to create a user with exist email", async() => {
+        const response = await request(app).post("/users").send({
+            email: "user@example.com",
+            name:"User Example"
+    });
+
+    expect(response.status).toBe(400)
+    });
 });
 
