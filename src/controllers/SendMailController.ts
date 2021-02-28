29256aa17a03f4acf448adf22b9305c526f8fbc6
emmanuel <oliveiravianaviana@gmail.com>
2021-02-28 11:42:20 -0300

    sendMail

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,9 +1,46 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
 
 
 class SendMailController {
-
     async execute(request: Request, response: Response) {
-        
+        const {email, survey_id} = request.body;
+
+        const userRepository = getCustomRepository(UsersRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const userAlreadyExists = await userRepository.findOne({email});
+
+        if(!userAlreadyExists) {
+            return response.status(400).json({
+                error: "User does not exists",
+            });
+        }
+
+        const survey = await surveysRepository.findOne({id: survey_id});
+
+        if(!survey) {
+            return response.status(400).json({
+                error: "Survey does not exists!"
+            })
+        }
+
+        //Salvar as informções na table surveyUser
+        const surveyUser = surveysUsersRepository.create({
+            user_id: userAlreadyExists.id,
+            survey_id
+        })
+        await surveysUsersRepository.save(surveyUser);
+        //Enviar email para o usuario
+
+        await SendMailService.execute(email, survey.title, survey.description);
+
+        return response.json(surveyUser);
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 import { SurveyController } from './controllers/SurveysController';
 import { UserController } from './controllers/userController';
+import {SendMailController} from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
 
+const sendMailController = new SendMailController();
+
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
